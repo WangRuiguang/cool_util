390e14b110a861bcbabf1b4e1e05e0c275480a7a
wangruiguang <wangruiguang@ftl-express.cn>
2021-06-21 17:23:50 +0800

    1.0.0

--- lib/main.ts
+++ lib/main.ts
@@ -18,10 +18,10 @@ export function HandleNone(index?: number) {
   return (_target: unknown, _propertyName: string, propertyDesciptor: PropertyDescriptor): PropertyDescriptor => {
     const method = propertyDesciptor.value
     propertyDesciptor.value = function (...args: any[]) {
-      const targetArg = args?.[index] ?? args[args.length-1]
+      const targetArg = args?.[index ?? args.length - 1]
       if (!Array.isArray(targetArg)) {
         throw new Error('argument found with index is not array!')
-      } else if (targetArg.length===0) {
+      } else if (targetArg.length === 0) {
         return []
       } else {
         return method.apply(this, args)
